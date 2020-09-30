b1d2c640cfc9b2f2ad30847768fde435798bde22
dreamyyyyyy <31621059+dreamyy1@users.noreply.github.com>
2020-09-30 11:05:28 +0200

    Update PetShopContext.cs

--- PetShop.Infrastructure.Database/PetShopContext.cs
+++ PetShop.Infrastructure.Database/PetShopContext.cs
@@ -12,6 +12,14 @@ namespace PetShop.Infrastructure.Database
         {
             
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pet>()
+                .HasOne(p => p.Owner)
+                .WithMany(o => o.Pets)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
         public DbSet<Pet> Pets { get; set; }
         public DbSet<Owner> Owners { get; set; }
         public DbSet<PetType> PetTypes { get; set; }
